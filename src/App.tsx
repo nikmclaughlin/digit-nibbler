8c01be212341cb35044a8a4768915b9598852cbf
Nik McLaughlin <72171383+nikmclaughlin@users.noreply.github.com>
2024-07-03 16:24:45 -0700

    Update TODOs

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,40 @@
+import GameGrid from './components/GameGrid'
 
 function App() {
-  
-  /** 
+  /**
    * TODO
-   * Spaces that can have digits
-   * A 5x5 grid of Spaces
-   * A random/seeded digit generator for each Space
-   * The Nibbler
-   * keyboard controls (arrows move, space to nibble)
-   * mouse/touch controls (press other space to move, press current space to nibble)
-   * A score system
-   * A check to see if the level is complete
-   * Generate a new level once complete
-   * Some instructions
-   * 
+   * -[X] Spaces that can have digits
+   * -[X] A 5x5 grid of Spaces
+   * -[X] A random/seeded digit generator for each Space
+   * -[X] The Nibbler
+   * -[ ] Nibbler moves between tiles
+   * -[ ] Nibbler eats number
+   * -[ ] keyboard controls (arrows move, space to nibble)
+   * -[ ] Movement follows grid (row/col traversal, edges)
+   * -[ ] mouse/touch controls (press other space to move, press current space to nibble)
+   * -[ ] A score system
+   * -[ ] A check to see if the level is complete
+   * -[ ] Generate a new level once complete
+   * -[ ] Some instructions
+   * ===================================
    * NICE TO HAVES
-   * Troggles!
-   * Multiple game modes (Multiples, Factors, Prime, Equality, Non-Equality, Challenge that changes modes each round)
-   * A menu system (Main, mode select, enter scores)
-   * High score (local)
-   * High score (global)
-   * Animations between rounds
+   * -[ ] Troggles!
+   * -[ ] Multiple game modes (Multiples, Factors, Prime, Equality, Non-Equality, Challenge that changes modes each round)
+   * -[ ] A menu system (Main, mode select, enter scores)
+   * -[ ] High score (local)
+   * -[ ] High score (global)
+   * -[ ] Animations between rounds
    */
-
 
   return (
     <main className="font-mono">
       <h1 className="text-3xl ">DIGIT NIBBLER</h1>
-
-      <div>
-        🥳 App successfully hosted.
+      <div id="gameContainer" className="w-fit border-2 border-green-500">
+        <GameGrid></GameGrid>
       </div>
+      <div>🥳 App successfully hosted.</div>
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
