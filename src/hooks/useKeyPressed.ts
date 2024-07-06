52c8e926378457466da3304b25434ed4800ab15b
Nik McLaughlin <72171383+nikmclaughlin@users.noreply.github.com>
2024-07-06 15:28:33 -0700

    add useKeyPressed hook

---
+++ src/hooks/useKeyPressed.ts
@@ -0,0 +1,36 @@
+import { useCallback, useEffect, useState } from 'react'
+
+export function useKeyPressed(targetKey: string) {
+  const [keyPressed, setKeyPressed] = useState(false)
+
+  const downHandler = useCallback(
+    (e: KeyboardEvent) => {
+      const { key } = e
+      if (key === targetKey) {
+        setKeyPressed(true)
+      }
+    },
+    [targetKey]
+  )
+
+  const upHandler = useCallback(
+    (e: KeyboardEvent) => {
+      const { key } = e
+      if (key === targetKey) {
+        setKeyPressed(false)
+      }
+    },
+    [targetKey]
+  )
+
+  useEffect(() => {
+    window.addEventListener('keydown', downHandler)
+    window.addEventListener('keyup', upHandler)
+    return () => {
+      window.removeEventListener('keydown', downHandler)
+      window.removeEventListener('keyup', upHandler)
+    }
+  }, [downHandler, upHandler])
+
+  return keyPressed
+}
