7bf95e90c549f0cdcbd7e934dd935625e63ca940
Nik McLaughlin <72171383+nikmclaughlin@users.noreply.github.com>
2024-07-08 07:37:41 -0700

    react router init

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
+import { Amplify } from 'aws-amplify'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import outputs from '../amplify_outputs.json'
+import App from './App.tsx'
+import './index.css'
 
-Amplify.configure(outputs);
+Amplify.configure(outputs)
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+])
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
-);
+)
